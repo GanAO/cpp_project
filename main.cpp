a4831fa8d9b0eb94546c5bed494867d053349047
Gan AO <njganao520fj@gmail.com>
2025-05-12 01:45:36 +0800

    Add square function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+int square(int x) {
+    return x * x;
+}
 int main() {
     std::cout << "Hello, Git!" << std::endl;
+    std::cout << "Updated code" << std::endl;
+    std::cout << "Square of 5: " << square(5) << std::endl;
     return 0;
 }
